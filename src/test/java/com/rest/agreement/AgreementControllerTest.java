b6ca07801d6ac9d50943a14322c08a397833de9c
Nguyen Dang Toan <nguyendangtritoan2305@gmail.com>
2021-01-31 23:02:44 +0200

    Add unit test for customer controller

---
+++ src/test/java/com/rest/agreement/AgreementControllerTest.java
@@ -0,0 +1,4 @@
+package com.rest.agreement;
+
+public class AgreementControllerTest {
+}
---
+++ src/test/java/com/rest/agreement/CustomerControllerTest.java
@@ -0,0 +1,97 @@
+package com.rest.agreement;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rest.agreement.controller.CustomerController;
+import com.rest.agreement.entity.CustomerEntity;
+import com.rest.agreement.repository.CustomerRepo;
+import com.rest.agreement.service.CustomerSer;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@WebMvcTest(CustomerController.class)
+public class CustomerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private CustomerRepo customerRepo;
+
+    @MockBean
+    private CustomerSer customerSer;
+
+    @Test
+    public void testListCustomers() throws Exception {
+        List<CustomerEntity> customerEntityList = new ArrayList<>();
+
+        customerEntityList.add(new CustomerEntity(1,"T015", "toan"));
+        customerEntityList.add(new CustomerEntity(2,"T016", "test1"));
+        customerEntityList.add(new CustomerEntity(3,"T017", "test2"));
+        customerEntityList.add(new CustomerEntity(4,"T018", "test3"));
+        customerEntityList.add(new CustomerEntity(5,"T019", "test4"));
+
+        Mockito.when(customerRepo.findAll()).thenReturn(customerEntityList);
+
+        String url = "/customer/list";
+
+        mockMvc.perform(get(url)).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testAddCustomer() throws Exception {
+        CustomerEntity newEntity = new CustomerEntity(1,"T015", "toan");
+
+        CustomerEntity savedEntity = new CustomerEntity(1,"T015", "toan");
+        Mockito.when(customerRepo.save(newEntity)).thenReturn(savedEntity);
+
+        String url = "/customer/save";
+        mockMvc.perform(post(url)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(newEntity))
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUpdateCustomer() throws Exception {
+        CustomerEntity newEntity = new CustomerEntity(1,"T015", "toan");
+
+        CustomerEntity savedEntity = new CustomerEntity(1,"T015", "toan");
+        Mockito.when(customerRepo.save(newEntity)).thenReturn(savedEntity);
+
+        String urlUpdate = "/customer/update";
+
+        mockMvc.perform(put(urlUpdate)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(newEntity))
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeleteCustomer() throws Exception {
+
+        long id = 1;
+        String urlDelete = "/customer/delete/"+id;
+
+        Mockito.doNothing().when(customerRepo).deleteById(id);
+
+        mockMvc.perform(delete(urlDelete)).andExpect(status().isOk());
+
+        Mockito.verify(customerRepo, Mockito.times(1)).deleteById(id);
+    }
+
+
+}
